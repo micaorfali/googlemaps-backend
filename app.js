330b12a53015b2da8a795e175393ffdaa00db781
micaorfali <mica.orfali@gmail.com>
2021-11-24 20:00:41 -0300

    mongo

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const express = require("express"),
 
   // Connection to DB
 mongoose.connect(
-  'mongodb+srv://maimo:dKiw8vPdNBfIQzRD@cluster0.jkpmu.mongodb.net/gmaps?retryWrites=true&w=majority',
+  'mongodb+srv://mica:mica@markers.jr9xp.mongodb.net/markers?retryWrites=true&w=majority',
   { useNewUrlParser: true, useUnifiedTopology: true },
   function (err, res) {
       try {
